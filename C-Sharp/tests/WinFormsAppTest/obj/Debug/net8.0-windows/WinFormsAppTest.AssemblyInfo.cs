6acbed30ee041a4dc11e0d64788690baba4176bb
SunRada <sun_da_lei@outlook.com>
2024-12-24 23:44:52 +0900

    first commit:add test project

--- C-Sharp/tests/WinFormsAppTest/obj/Debug/net8.0-windows/WinFormsAppTest.AssemblyInfo.cs
+++ C-Sharp/tests/WinFormsAppTest/obj/Debug/net8.0-windows/WinFormsAppTest.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("SampleWinForm")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsAppTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5b03e19dec9797dcda2c8d9f548153c86ed031e")]
-[assembly: System.Reflection.AssemblyProductAttribute("SampleWinForm")]
-[assembly: System.Reflection.AssemblyTitleAttribute("SampleWinForm")]
+[assembly: System.Reflection.AssemblyProductAttribute("WinFormsAppTest")]
+[assembly: System.Reflection.AssemblyTitleAttribute("WinFormsAppTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
