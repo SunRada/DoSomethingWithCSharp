585dcc969426cfdd1887a38903cba8e952cfebbb
SunRada <sun_da_lei@outlook.com>
2025-05-12 00:32:23 +0900

    Added Property Setting Screan Form For add toolbox in later.

--- src/SampleApp/SampleWinForm/Main.Designer.cs
+++ src/SampleApp/SampleWinForm/Main.Designer.cs
@@ -28,7 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            checkBox1 = new CheckBox();
+            groupBox1 = new GroupBox();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            groupBox1.SuspendLayout();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(1267, 669);
+            button1.Name = "button1";
+            button1.Size = new Size(131, 40);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(56, 61);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(145, 32);
+            checkBox1.TabIndex = 1;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = SystemColors.InactiveCaption;
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(checkBox1);
+            groupBox1.Location = new Point(57, 62);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(414, 269);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(1436, 669);
+            button2.Name = "button2";
+            button2.Size = new Size(131, 40);
+            button2.TabIndex = 3;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(201, 121);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(175, 34);
+            textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(56, 124);
+            label1.Name = "label1";
+            label1.Size = new Size(73, 28);
+            label1.TabIndex = 3;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(56, 197);
+            label2.Name = "label2";
+            label2.Size = new Size(73, 28);
+            label2.TabIndex = 5;
+            label2.Text = "label2";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(201, 194);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(175, 34);
+            textBox2.TabIndex = 4;
             // 
             // Main
             // 
@@ -36,11 +120,25 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Window;
             ClientSize = new Size(1656, 986);
+            Controls.Add(button2);
+            Controls.Add(groupBox1);
+            Controls.Add(button1);
             Name = "Main";
             Text = "Do Something？";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private CheckBox checkBox1;
+        private GroupBox groupBox1;
+        private Label label1;
+        private TextBox textBox1;
+        private Button button2;
+        private Label label2;
+        private TextBox textBox2;
     }
 }
